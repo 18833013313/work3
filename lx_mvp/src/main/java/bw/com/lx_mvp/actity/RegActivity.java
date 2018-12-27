cb106c4b2d2bbabfdca5f673f22eb361ac8695f9
18833013313 <2417539426@qq.com>
2018-12-27 21:06:38 +0800

    xiugai

--- lx_mvp/src/main/java/bw/com/lx_mvp/actity/RegActivity.java
+++ lx_mvp/src/main/java/bw/com/lx_mvp/actity/RegActivity.java
@@ -3,12 +3,14 @@ package bw.com.lx_mvp.actity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 import bw.com.lx_mvp.R;
+import bw.com.lx_mvp.view.user.IRegVIew;
 
-public class RegActivity extends AppCompatActivity {
+public class RegActivity extends AppCompatActivity implements IRegVIew {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +31,20 @@ public class RegActivity extends AppCompatActivity {
         params.put("password","111111");
 
 
+    }
+
+    @Override
+    public void mobileError(String error) {
+        Toast.makeText(this,"mobileError",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void success(String result) {
+        Toast.makeText(this,"success",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void failure(String msg) {
+        Toast.makeText(this,"failure",Toast.LENGTH_SHORT).show();
     }
 }
